f561dae7db9aacfbf2bb80509b8ce99bbc9e1cf1
Sean McPherson <git@seanmcp.com>
2021-04-02 22:43:12 -0400

    feat: reorder items

--- main.js
+++ main.js
@@ -21,17 +21,38 @@
       lists[type].textContent = "";
       items.forEach((item) => {
         const li = document.createElement("li");
-        li.classList.add("p-2", "flex", "justify-between")
+        li.classList.add("p-2", "flex", "justify-between");
         const span = document.createElement("span");
         span.textContent = item.item;
         li.appendChild(span);
+
+        const controlsDiv = document.createElement("div");
+        li.appendChild(controlsDiv);
+
+        const upButton = document.createElement("button");
+        upButton.textContent = "Up";
+        upButton.addEventListener("click", (event) => {
+          event.preventDefault();
+          move(item.id, type, "up");
+        });
+        controlsDiv.appendChild(upButton);
+
+        const downButton = document.createElement("button");
+        downButton.textContent = "Down";
+        downButton.addEventListener("click", (event) => {
+          event.preventDefault();
+          move(item.id, type, "down");
+        });
+        controlsDiv.appendChild(downButton);
+
         const deleteButton = document.createElement("button");
         deleteButton.textContent = "Delete";
         deleteButton.addEventListener("click", (event) => {
           event.preventDefault();
           remove(item.id, type);
         });
-        li.appendChild(deleteButton);
+        controlsDiv.appendChild(deleteButton);
+
         lists[type].appendChild(li);
       });
     });
@@ -64,6 +85,31 @@
       id: String(new Date().getTime()).slice(-5),
       item,
     });
+    localStorage.setItem(type, JSON.stringify(store));
+    refreshUi();
+  }
+
+  function move(id, type, direction) {
+    const store = JSON.parse(localStorage.getItem(type) || "[]");
+    const length = store.length;
+    const index = store.findIndex((item) => item.id === id);
+    const [item] = store.splice(index, 1);
+    let nextIndex = index;
+
+    switch (direction) {
+      case "up": {
+        if (index === 0) return;
+        nextIndex = index - 1;
+        break;
+      }
+      case "down": {
+        if (index === length - 1) return;
+        nextIndex = index + 1;
+        break;
+      }
+    }
+
+    store.splice(nextIndex, 0, item);
     localStorage.setItem(type, JSON.stringify(store));
     refreshUi();
   }
